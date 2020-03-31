63885d191ceb5f76f244435c524de67029b115f0
Mohamed Nuh <mojitsu185@gmail.com>
2020-03-31 12:52:59 -0700

    2

--- js/script.js
+++ js/script.js
@@ -30,17 +30,9 @@ const names = document.querySelectorAll('.student-details')
 /*** 
    Create the `showPage` function to hide all of the items in the 
    list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
 ***/
+
+
 
 
 
