8b1829bf4219372135f57c075ebf46d71aef7562
Mohamed Nuh <mojitsu185@gmail.com>
2020-03-31 13:01:46 -0700

    23

--- js/script.js
+++ js/script.js
@@ -32,8 +32,20 @@ const names = document.querySelectorAll('.student-details')
    list except for the ten you want to show.
 ***/
 
+const show = (listedPeople,page) => {
+   const begin = page * items - items;
+   const end = page * items;
 
+   for (let i = 0; i < listedPeople.length; i++) {
+      if (i >= begin && i <end) {
+         listedPeople[i].style.display = 'block'
+      } else {
+         listedPeople[i].style.display = 'none'
+      }
+   }
+}
 
+show(listedPeople,1)
 
 
 
